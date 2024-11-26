739bf8fcfac8330fd0a31cf69a4bdc23d6494a3a
Keibol <jjendo@protonmail.com>
2024-11-26 20:18:42 +0100

    feat: add db and users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
+import { WarehouseModule } from './warehouse/warehouse.module';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [WarehouseModule],
+  controllers: [,],
+  providers: [],
 })
 export class AppModule {}
