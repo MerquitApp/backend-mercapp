5038fad11cacd32ba9e74904a7d8502464cf8fbb
JJ Endo <155273455+JJEndo@users.noreply.github.com>
2024-11-28 09:47:11 +0100

    Delete src/app.module.ts

--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-
-@Module({
-  imports: [UsersModule, AuthModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
