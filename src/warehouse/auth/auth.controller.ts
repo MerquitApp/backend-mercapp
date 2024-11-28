fd9582f0bde5da6416231b3f5f39f3205ca26f1a
Andy Uzal <andyuzal@gmail.com>
2024-11-28 12:46:25 +0100

    refactor: change auth logic

--- src/warehouse/auth/auth.controller.ts
+++ src/warehouse/auth/auth.controller.ts
@@ -1,11 +1,31 @@
-import { Body, Controller, Post, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login-user.dto';
 import { Request, Response } from 'express';
+import { RegisterDto } from './dto/register.dto';
+import { Role } from '../common/enums/rol.enum';
+import { Auth } from './decorators/auth.decorator';
+import { ActiveUser } from '../common/decorators/active-user.decorator';
+import { UserActiveInterface } from '../common/interfaces/user-active.interface';
+
+interface RequestWithUser extends Request {
+  user: {
+    email: string;
+    role: string;
+  };
+}
 
 @Controller('/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  register(
+    @Body()
+    registerDto: RegisterDto,
+  ) {
+    return this.authService.register(registerDto);
+  }
 
   @Post('/login')
   async login(
@@ -26,5 +46,12 @@ export class AuthController {
         message: 'Error interno de servidor!',
       });
     }
+  }
+
+  @Get('profile')
+  @Auth(Role.USER)
+  profile(@ActiveUser() user: UserActiveInterface) {
+    console.log(user);
+    return this.authService.profile(user);
   }
 }
--- src/warehouse/auth/auth.module.ts
+++ src/warehouse/auth/auth.module.ts
@@ -1,15 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtStrategy } from './jwt.strategy';
-import { PassportModule } from '@nestjs/passport';
-import { PrismaService } from 'src/prisma.service';
-import { UsersService } from '../users/users.service';
 import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants/jwt.constant';
 
 @Module({
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, PrismaService, JwtStrategy, UsersService],
-  imports: [UsersModule, PassportModule],
+  providers: [AuthService],
 })
 export class AuthModule {}
