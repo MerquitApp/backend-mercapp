411eb5f1c76c0840e4ac26c98b2b80724bae4f9c
Hugo Boyano <hnboyano@gmail.com>
2024-12-07 00:59:55 +0100

    add webrtc

--- src/chat-ws/chat-ws.gateway.ts
+++ src/chat-ws/chat-ws.gateway.ts
@@ -5,6 +5,7 @@ import {
   WebSocketGateway,
 } from '@nestjs/websockets';
 import { ChatWsService } from './chat-ws.service';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway()
 export class ChatWsGateway {
@@ -13,10 +14,73 @@ export class ChatWsGateway {
   @SubscribeMessage('message')
   handleMessage(
     @ConnectedSocket()
-    client: any,
+    client: Socket,
     @MessageBody()
     message: string,
   ) {
     return this.chatWsService.handleMessage(client, message);
+  }
+
+  @SubscribeMessage('join-call')
+  handleJoinCall(
+    @ConnectedSocket()
+    client: Socket,
+    @MessageBody()
+    message: string,
+  ) {
+    const data = JSON.parse(message);
+    return this.chatWsService.handleJoinCall(client, data.callId);
+  }
+
+  @SubscribeMessage('ice-offer')
+  handleWebrtcICECandidateOffer(
+    @ConnectedSocket()
+    client: Socket,
+    @MessageBody()
+    message: string,
+  ) {
+    const data = JSON.parse(message);
+    this.chatWsService.handleICECandidateOffer(
+      client,
+      data.callId,
+      data.candidate,
+    );
+  }
+
+  @SubscribeMessage('ice-answer')
+  handleWebrtcICECandidate(
+    @ConnectedSocket()
+    client: Socket,
+    @MessageBody()
+    message: string,
+  ) {
+    const data = JSON.parse(message);
+    this.chatWsService.handleICECandidateAnswer(
+      client,
+      data.callId,
+      data.candidate,
+    );
+  }
+
+  @SubscribeMessage('offer')
+  handleWebrtcOffer(
+    @ConnectedSocket()
+    client: Socket,
+    @MessageBody()
+    message: string,
+  ) {
+    const data = JSON.parse(message);
+    return this.chatWsService.handleOffer(data.callId, data.offer);
+  }
+
+  @SubscribeMessage('answer')
+  handleWebrtcAnswer(
+    @ConnectedSocket()
+    client: Socket,
+    @MessageBody()
+    message: string,
+  ) {
+    const data = JSON.parse(message);
+    return this.chatWsService.handleAnswer(client, data.callId, data.answer);
   }
 }
