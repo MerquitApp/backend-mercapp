414b87a48e4e9cd491b09c04f1ec5ffb1ed20023
KeibolNegro <155273455+KeibolNegro@users.noreply.github.com>
2025-02-16 11:53:08 +0100

    feat:add documentation reputation system

--- src/reputation/reputation.controller.ts
+++ src/reputation/reputation.controller.ts
@@ -3,13 +3,25 @@ import { ReputationService } from './reputation.service';
 import { CreateReputationDto } from './dto/create-reputation.dto';
 import { Reputation } from '@prisma/client';
 import { Body, Get, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CreateCategoryDto } from 'src/categories/dto/create-category.dto';
 
+@ApiTags('reputation')
 @Controller('reputation')
 export class ReputationController {
   constructor(private readonly reputationService: ReputationService) {}
 
   // Endpoint para crear una nueva reputación
   @Post()
+  @ApiOperation({
+    summary: 'Crear reputación',
+    description: 'Crea una nueva reputación en el sistema.',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Reputación creada exitosamente.',
+    type: CreateCategoryDto,
+  })
   async create(
     @Body() createReputationDto: CreateReputationDto,
   ): Promise<Reputation> {
@@ -18,6 +30,16 @@ export class ReputationController {
 
   // Endpoint para obtener todas las reputaciones
   @Get()
+  @ApiOperation({
+    summary: 'Obtener reputación',
+    description:
+      'Obtiene la reputación asociada al ID proporcionado en el cuerpo de la solicitud.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Reputación encontrada.',
+    type: CreateCategoryDto,
+  })
   async findAll(@Body('id') id: number): Promise<Reputation> {
     return this.reputationService.getReputation(id);
   }
